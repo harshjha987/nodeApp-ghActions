dc575d5e361be69133305084284eb737deee02af
harshjha987 <jha.harsh837@gmail.com>
2026-02-01 21:56:27 +0530

    checking gh action working or not

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT ?? 8080;
 
 app.get("/",(req,res)=>{
-    return res.json({msg : "Hello from server v1"});
+    return res.json({msg : "Hello from server v1 and bye."});
 })
 
 app.listen(PORT,()=>{
